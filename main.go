ccf53534447b423554859229d0aec3540b6dc62e
kshyst <kiarash.sh1383@gmail.com>
2025-07-15 13:52:12 +0330

    Added Post block and main.go

--- main.go
+++ main.go
@@ -1,56 +1,24 @@
 package main
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/joho/godotenv"
+	"log"
 	"time"
 )
 
-type Block struct {
-	Index     int32  // is the position of the data record in the blockchain
-	Timestamp string // is automatically determined and is the time the data is written
-	BPM       int32  // or beats per minute, is your pulse rate
-	Hash      string // is a SHA256 identifier representing this data record
-	PrevHash  string // is the SHA256 identifier of the previous record in the chain
-}
-
-var Blockchain []Block
-
-func calculateHash(block Block) string {
-	record := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
-	h := sha256.New()
-	h.Write([]byte(record))
-	hashed := h.Sum(nil)
-	return hex.EncodeToString(hashed)
-}
-
-func generateBlock(oldBlock Block, BPM int32) (Block, error) {
-
-	var newBlock Block
-
-	t := time.Now()
-
-	newBlock.Index = oldBlock.Index + 1
-	newBlock.Timestamp = t.String()
-	newBlock.BPM = BPM
-	newBlock.PrevHash = oldBlock.Hash
-	newBlock.Hash = calculateHash(newBlock)
-
-	return newBlock, nil
-}
-
-func isBlockValid(newBlock, oldBlock Block) bool {
-	if oldBlock.Index+1 != newBlock.Index {
-		return false
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	if oldBlock.Hash != newBlock.PrevHash {
-		return false
-	}
+	go func() {
+		t := time.Now()
+		genesisBlock := Block{0, t.String(), 0, "", ""}
+		spew.Dump(genesisBlock)
+		Blockchain = append(Blockchain, genesisBlock)
+	}()
+	log.Fatal(run())
 
-	if calculateHash(newBlock) != newBlock.Hash {
-		return false
-	}
-
-	return true
 }
